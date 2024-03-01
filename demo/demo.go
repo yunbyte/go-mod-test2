5f18db832aaae45a700dda4ae011a07f0ad2feeb
yunkai <k1510207@163.com>
2024-03-01 17:04:37 +0800

    first commit

---
+++ demo/demo.go
@@ -0,0 +1,11 @@
+package demo
+
+import "fmt"
+
+func DemoFunc() {
+	fmt.Println("I'm DemoFunc of go-mod-test2")
+}
+
+func DemoFunc2() {
+	fmt.Println("I'm DemoFunc2 of go-mod-test2")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yunbyte/go-mod-test1
+
+go 1.21.6
